c19577e781a17a7136a65f154ced6134bc17977a
James_Cox <Jecox19@gmail.com>
2019-08-03 17:50:23 +0100

    Initial Git Commit - New Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace theoretical_toast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
