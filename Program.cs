92d725c78fd52e4755f8fbdb893102e3cbb76ead
James_Cox <Jecox19@gmail.com>
2019-08-03 17:59:00 +0100

    FizzBuzz Skeleton Created

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace theoretical_toast
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      int targetNumber = 100, firstMultiple = 3, secondMultiple = 5;
+      for (int currentNumber = 0; currentNumber <= targetNumber; currentNumber++)
+      {
+        if (currentNumber % firstMultiple == 0 && currentNumber % secondMultiple == 0)
         {
-            Console.WriteLine("Hello World!");
+
         }
+        else if (currentNumber % firstMultiple == 0)
+        {
+
+        }
+        else if (currentNumber % secondMultiple == 0)
+        {
+
+        }
+        else
+        {
+
+        }
+      }
     }
+  }
 }
